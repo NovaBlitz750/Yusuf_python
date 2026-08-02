89980e8a50ca5e0f593d4765139f3da724d4885e
Yichen787 <yichenwangnz@outlook.com>
2026-08-02 21:16:10 +1200

    Refactor weather prediction logic into a function

--- weatherpre.py
+++ weatherpre.py
@@ -1,12 +1,16 @@
 weather=(1,0,0,1,1,0,1,0,1,1,0)
 r=0
 s=0
+def weatherpre():
+    if r>s:
+        print("It will be rainy")
+    else:
+        print("It will be sunny")
+
 for i in range(0,len(weather)):
     if weather[i]==1:
         r+=1
+        weatherpre()
     else:
-        s=+1
-if r>s:
-    print("It will be rainy ")
-else:
-    print("It will be sunny")
+        s+=1
+        weatherpre()
